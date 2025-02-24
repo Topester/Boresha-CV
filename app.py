48f26d16fd976549b45f1f9d63247ab96109ae18
Topester <muhonjaa.tops10@gmail.com>
2025-02-24 23:57:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ st.markdown('<div class="outer-container">', unsafe_allow_html=True)
 # Main content container
 st.markdown('<div class="main-container">', unsafe_allow_html=True)
 
-image_path = os.path.join("images/WhatsApp Image 2025-02-24 at 04.10.26 ")
+image_path = https://github.com/Topester/Boresha-CV/blob/main/WhatsApp%20Image%202025-02-24%20at%2004.10.26.jpeg?raw=true
 
 # Create two columns
 col1, col2 = st.columns([1, 2])  # Adjust width ratio as needed
